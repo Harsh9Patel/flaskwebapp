ab2839c2bf9de6ad734f718029d5b3d43edf4ec4
Harsh9Patel <91564311+Harsh9Patel@users.noreply.github.com>
2021-12-16 00:53:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def root():
-    return "Hello"
+@app.route('/index')
+def index():
+    return '<h1>Hello Harsh</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
