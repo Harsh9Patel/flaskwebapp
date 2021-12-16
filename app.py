65bdb99744f5b6704998b25fa0a216425d8b65ea
Harsh9Patel <91564311+Harsh9Patel@users.noreply.github.com>
2021-12-16 01:12:51 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-@app.route('/index')
-def index():
-    return '<h1>Hello Harsh</h1>'
-
-if __name__ == '__main__':
-    app.run(debug=True)
